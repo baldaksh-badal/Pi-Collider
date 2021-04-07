0f37cb2f416a13156f07e6c3d3d6152183616eea
baldaksh-badal <32961135+baldaksh-badal@users.noreply.github.com>
2021-04-07 22:11:21 +0530

    Update Blocks.py

--- Blocks.py
+++ Blocks.py
@@ -1,21 +1,4 @@
 import pygame
-import math
-
-
-WIN_X = 1000
-WIN_Y = 500
-
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
-BLUE = (0, 0, 255)
-
-
-win = pygame.display.set_mode((WIN_X, WIN_Y))
-pygame.init()
-pygame.display.set_caption("π collider")
-
 
 def calc_velocities(v1, v2, m1, m2) -> tuple:
     """Returns the final velocities of two blocks after a perfectly elastic
@@ -27,59 +10,66 @@ def calc_velocities(v1, v2, m1, m2) -> tuple:
     return add + base, base
 
 
-class Block(pygame.sprite.Sprite):
+class Block:
     def __init__(self, x, y, velocity, mass, width, height):
-        pygame.sprite.Sprite.__init__(self)
         self.x = x
         self.y = y
         self.velocity = velocity
         self.mass = mass
         self.width = width
         self.height = height
-        self.image = pygame.Surface([width, height])
-        self.rect = self.image.get_rect()
 
-    def draw(self, window, color):
-        pygame.draw.rect(window, color, (self.x, self.y, self.width, self.height))
-        pygame.display.update()
+def run_collisions(m1, m2):
+    WIN_X = 1000
+    WIN_Y = 500
 
-    def move(self):
-        self.x += self.velocity
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    GREEN = (0, 255, 0)
+    RED = (255, 0, 0)
+    BLUE = (0, 0, 255)
+
+    win = pygame.display.set_mode((WIN_X, WIN_Y))
+    pygame.init()
+    pygame.display.set_caption("π collider")
 
 
+    B1 = Block(x=250, y=0, velocity=0, mass=m1, width=20, height=20)
+    B2 = Block(x=500, y=0, velocity=-1, mass=m2, width=20, height=20)
 
-def main():
-    B1 = Block(x=(WIN_X / 2), y=0, velocity=0, mass=1, width=20, height=20)
-    B2 = Block(x=WIN_X-20, y=0, velocity=-1, mass=100, width=20, height=20)
     run = True
+    collision_count = 0
+
     while run:
-        pygame.time.delay(10)
-        win.fill(BLACK)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
+
         if B2.velocity > B1.velocity >= 0:
             run = False
         else:
+            pygame.time.delay(0)
+            win.fill(BLACK)
 
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+            pygame.draw.rect(win, GREEN, (B1.x, B1.y, B1.width, B1.height))
+            pygame.draw.rect(win, RED, (B2.x, B2.y, B2.width, B2.height))
 
-            if math.ceil(B1.x + B1.width) == math.ceil(B2.x):
+            if B1.x + (B1.width / 2) >= B2.x - (B2.width / 2):
                 B1.velocity, B2.velocity = calc_velocities(B1.velocity, B2.velocity, B1.mass, B2.mass)
+                collision_count += 1
 
-            elif math.floor(B1.x) < 0:
+            elif B1.x < 0:
                 B1.velocity = -B1.velocity
+                collision_count += 1
 
-            B1.draw(win, GREEN)
-            B1.move()
+            B1.x += B1.velocity
+            B2.x += B2.velocity
 
-            B2.draw(win, RED)
-            B2.move()
-
-
+            pygame.display.update()
+            print(collision_count)
 
     pygame.quit()
-
-
+    print(collision_count)
 
 if __name__ == '__main__':
-    main()
+    run_collisions(1, 100000000)
